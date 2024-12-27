398a23272478881141b452d5cef3e7fd6539142b
Felix O'Mahony <felixomahony@gmail.com>
2024-12-27 18:22:52 +0000

    edit-padding

--- components/Article.tsx
+++ components/Article.tsx
@@ -15,11 +15,11 @@ export default function Article({ children, title, date, link }: ArticleProps) {
         <h1 className={`font-bold mt-2 text-teal-950`}>{title}</h1>
         <h1 className={`italic mt-2 text-teal-950`}>{date}</h1>
       </div>
-      <p className="text-black mb-2 mt-2">{children}</p>
+      <p className="text-black mb-1 mt-2">{children}</p>
       <a
         href={link}
         target="_blank"
-        className="flex items-center text-blue-600 hover:underline font-semibold mb-4"
+        className="flex items-center text-blue-600 hover:underline font-semibold mb-2"
       >
         ➔ More
       </a>
